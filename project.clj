96e738b0e9afd933db72cba0d4b1e77a9c88e166
Kamil Szymczyk <kaszymczyk@gmail.com>
2014-04-17 22:13:20 +0200

    Update to Clojure 1.6.0 and libraries clj-http and commons-codec.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject fablo/api-client "unused"
   :main fablo.api-client-main
   :description "Fablo API client"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-json "0.5.3"]
                  [commons-io/commons-io "2.4"]
-                 [commons-codec "1.8"]
-                 [clj-http "0.7.8"]
+                 [commons-codec "1.9"]
+                 [clj-http "0.9.1"]
                  [ring/ring-codec "1.0.0"]
                  [clojopts "0.3.2"]     ; move to tools.cli
                  [clj-time "0.6.0"]]
